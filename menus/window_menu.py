cf847e7cd28700fbcb87c280adcdf8eb33159920
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-07-26 21:53:25 +0100

    Add toggle for Nodes Palette to 'Window' menu

--- menus/window_menu.py
+++ menus/window_menu.py
@@ -13,6 +13,8 @@ class WindowMenu(QtWidgets.QMenu):
         self.create_connections()
 
     def create_actions(self):
+        self.nodes_palette_action = QtWidgets.QAction('Nodes Palette', self)
+        self.nodes_palette_action.setCheckable(True)
         self.close_current_action = QtWidgets.QAction('Close', self)
         self.close_all_action = QtWidgets.QAction('Close All', self)
         self.tile_action = QtWidgets.QAction('Tile', self)
@@ -25,6 +27,7 @@ class WindowMenu(QtWidgets.QMenu):
     def create_connections(self):
         self.aboutToShow.connect(self.update_actions_state)
         # Actions
+        self.nodes_palette_action.triggered.connect(self.toggle_nodes_palette)
         self.close_current_action.triggered.connect(self.main_dialog.mdi_area.closeActiveSubWindow)
         self.close_all_action.triggered.connect(self.main_dialog.mdi_area.closeAllSubWindows)
         self.tile_action.triggered.connect(self.main_dialog.mdi_area.tileSubWindows)
@@ -32,6 +35,8 @@ class WindowMenu(QtWidgets.QMenu):
         self.previous_wnd_action.triggered.connect(self.main_dialog.mdi_area.activatePreviousSubWindow)
 
     def populate(self):
+        self.addAction(self.nodes_palette_action)
+        self.addSeparator()
         self.addAction(self.close_current_action)
         self.addAction(self.close_all_action)
         self.addSeparator()
@@ -41,9 +46,13 @@ class WindowMenu(QtWidgets.QMenu):
         self.addAction(self.previous_wnd_action)
 
     def update_actions_state(self):
+        self.nodes_palette_action.setChecked(self.main_dialog.nodes_palette.isVisible())
         has_mdi_child = self.main_dialog.current_editor is not None
         self.close_current_action.setEnabled(has_mdi_child)
         self.close_all_action.setEnabled(has_mdi_child)
         self.tile_action.setEnabled(has_mdi_child)
         self.next_wnd_action.setEnabled(has_mdi_child)
         self.previous_wnd_action.setEnabled(has_mdi_child)
+
+    def toggle_nodes_palette(self):
+        self.main_dialog.nodes_palette.setVisible(not self.main_dialog.nodes_palette.isVisible())

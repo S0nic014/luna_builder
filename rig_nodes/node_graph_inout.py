58e9b03926c1dbe00e3ccdb733ef96119513e6d1
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-08-02 22:46:33 +0100

    Implement exec method for Output node

--- rig_nodes/node_graph_inout.py
+++ rig_nodes/node_graph_inout.py
@@ -2,6 +2,7 @@
 import pymel.core as pm
 import luna.workspace
 from luna import Logger
+import luna.utils.maya_utils as maya_utils
 import luna_rig.functions.asset_files as asset_files
 import luna_builder.editor.editor_conf as editor_conf
 import luna_builder.rig_nodes.luna_node as luna_node
@@ -48,7 +49,24 @@ class GraphOutputNode(luna_node.LunaNode):
 
     def init_sockets(self, inputs=[], outputs=[], reset=True):
         self.exec_in_socket = self.add_input(editor_conf.DataType.EXEC)
+        self.in_character = self.add_input(editor_conf.DataType.CHARACTER, label='Character')
+
         self.exec_out_socket = None
+
+    def execute(self):
+        try:
+            self.in_character.value.save_bind_pose()
+
+            # Adjust viewport
+            pm.select(cl=1)
+            maya_utils.switch_xray_joints()
+            pm.viewFit(self.in_character.value.root_control.group)
+            self.in_character.value.geometry_grp.overrideEnabled.set(1)
+            self.in_character.value.geometry_grp.overrideColor.set(1)
+            return 0
+        except Exception:
+            Logger.exception('Failed to exec {} node'.format(self.title))
+            return 1
 
 
 def register_plugin():
